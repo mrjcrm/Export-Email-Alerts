9ada5fb0f472eeddfb8e7e3c3b24735cdbfc062f
Rod MacKenzie <mr.jcrm@gmail.com>
2026-02-24 13:48:22 -0500

    2026-02-24 minor stuff

--- scripts/sfcli/1 Create pkg.cmd
+++ scripts/sfcli/1 Create pkg.cmd
@@ -1,4 +1,4 @@
 REM To prevent autoupdate of sfdx-project.json during package create, set the following windows env variable:
 REM set SF_PROJECT_AUTOUPDATE_DISABLE_FOR_PACKAGE_CREATE=true
 
-sf package create --name="Export Email Alerts" --package-type="Unlocked" --path="force-app" --target-dev-hub="mr.jcrm.devhub@gmail.com" --error-notification-username="mr.jcrm.devhub@gmail.com" --description="List all email alerts in CSV format"
+sf package create --name="Export Email Alerts" --package-type="Unlocked" --path="force-app" --target-dev-hub="mr.jcrm.devhub@gmail.com" --error-notification-username="mr.jcrm.devhub@gmail.com" --description="Finally, a list of all the email alerts in your org!"
--- scripts/sfcli/2 Create pkg ver.cmd
+++ scripts/sfcli/2 Create pkg ver.cmd
@@ -1,4 +1,4 @@
 REM To prevent autoupdate of sfdx-project.json during package versioncreate, set the following windows env variable:
 REM set SF_PROJECT_AUTOUPDATE_DISABLE_FOR_PACKAGE_VERSION_CREATE=true
 
-sf package version create --installation-key-bypass --package=0Hoak0000001E8zCAE --wait="20" --version-description="The only way to see all the email alerts in Salesforce!" --version-name="Final" --code-coverage --path="force-app" --verbose --target-dev-hub="mr.jcrm.devhub@gmail.com" --tag="gitid 1234567" --releasenotes-url="https://github.com/mrjcrm/Export-Email-Alerts"
+sf package version create --installation-key-bypass --package=0Hoak0000001E8zCAE --wait="20" --version-description="Finally, a list of all the email alerts in your org!" --version-name="Final" --code-coverage --path="force-app" --verbose --target-dev-hub="mr.jcrm.devhub@gmail.com" --tag="gitid 1234567" --releasenotes-url="https://github.com/mrjcrm/Export-Email-Alerts"
